1cb9bc3ce25c3de50c5bd5c1f4fb467f2e8be541
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2024-01-31 05:42:44 +0700

    countDown Pemilu

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
-const countdownDate = new Date("Feb 14, 2024 00:00:00");
+const countdownDate = new Date("Feb 14, 2024 00:00:00").getTime();
+
+const now = new Date().getTime();
+
+const distance = countdownDate - now;
+
+document.write(distance);
