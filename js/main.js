439a0e17fdbe11afe4b306a97ecbf50d588ac1ca
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2024-01-31 09:45:28 +0700

    countDown Pemilu

--- js/main.js
+++ js/main.js
@@ -1,9 +1,25 @@
-const countdownDate = new Date("Feb 14, 2024 00:00:00").getTime();
+const countdownDate = new Date("FEB 14, 2024 00:00:00").getTime();
 
-const now = new Date().getTime();
+const x = setInterval(function() {
+    const now = new Date().getTime();
 
-const distance = countdownDate - now;
+    const distance = countdownDate - now;
 
-const days = distance / (1000 * 60 * 60 * 24);
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
 
-document.write(days);
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // document.write("days", ":", "hours", ":", "minutes", ":", "seconds");
+    // document.write("<br>");
+    // document.write(days, ":", hours, ":", minutes, ":", seconds);
+
+    document.getElementById("days").innerHTML = days;
+    document.getElementById("hours").innerHTML = hours;
+    document.getElementById("minutes").innerHTML = minutes;
+    document.getElementById("seconds").innerHTML = seconds;
+}, 1000);
+
